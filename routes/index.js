3d1a9f0835678d48765f767b3437af13b7a2a34a
shubhshrma <shubham.31996@gmail.com>
2017-08-18 19:19:02 +0530

    update files

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,33 @@ router.get('/', function(req, res, next) {
 
 //GET Timestamp
 router.get('/:date', function(req,res){
-	console.log(1);
-	res.render('index');
+	var date = req.params.date;
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", 
+    "August", "September", "October", "November", "December"];
+	
+	if(isNaN(date)){
+       var parsed = Date.parse(date);
+       
+       if(parsed){
+       	  var date_obj=new Date(parsed);
+       	  var naturalDate=" "+date_obj.getDate()+" "+months[date_obj.getMonth()]+", "+date_obj.getFullYear();
+       	  var unixDate=date_obj.getTime()/1000;
+       }
+       else{
+           var naturalDate=null;
+           var unixDate=null;
+       }
+	}
+	else{
+		var date_obj=new Date(1000*date);
+       	var naturalDate=" "+date_obj.getDate()+" "+months[date_obj.getMonth()]+", "+date_obj.getFullYear();
+
+        var unixDate=date_obj.getTime()/1000;
+		 
+	}
+	res.json({naturalDate:naturalDate,unixDate:unixDate});
+	
 });
 
 module.exports = router;
